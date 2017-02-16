5e1567edd1100f4546016da2f692865e04b51dd2
arqetype <arq@telenet.be>
2017-02-16 22:59:52 +0100

    GUI exercise with loops drawing lines in a window.

---
+++ ch05/ArtComponent.java
@@ -0,0 +1,84 @@
+package ch05;
+
+import javax.swing.*;
+
+import java.awt.*;
+
+/**	Create a component that paints our 'art'
+ * 
+ * @author arq
+ *
+ */
+
+public class ArtComponent extends JComponent {
+	public ArtComponent() {
+		super();
+		this.setPreferredSize(new Dimension(500,500));
+	}
+	
+	/**	Paint the component with our 'art' */
+	public void paintComponent(Graphics g) {
+		super.paintComponent(g);
+		// Standard stuff to scale teh image.
+		Graphics2D g2 = (Graphics2D) g;
+		g2.scale(this.getWidth()/11, this.getHeight()/11);
+		g2.setStroke(new BasicStroke(1.0F/this.getWidth()));
+		g2.setColor(Color.ORANGE);
+		g2.fillRect(0, 0, 11, 11);
+
+		// draw our 'art'
+		for (int left = 1; left <= 10; left += 1) {
+			if (left % 2 == 0) {
+				g2.setColor(Color.YELLOW);
+			} else {
+				g2.setColor(Color.BLUE);
+			}
+			
+			for (int right = 1; right <= 10; right += 1) {
+				g2.drawLine(1, left, 10, right);
+			}
+		}
+
+//		for (int left = 1; left <= 10; left += 1) {
+//			for (int right = 1; right <= 10; right += 1) {
+//				if (left + right > 10) {
+//					g2.setColor(Color.RED);
+//				} else {
+//					g2.setColor(Color.BLUE);
+//				}
+//				
+//				g.drawLine(1, left, 10, right);
+//			}
+//		}
+
+//		for (int left = 1; left <= 10; left += 1) {
+//			if (left <= 5) {
+//				g2.setColor(Color.WHITE);
+//			} else {
+//				g2.setColor(Color.BLACK);
+//			}
+//			
+//			for (int right = 1; right <= 10; right += 1) {
+//				g.drawLine(1, left, 10, right);
+//			}
+//		}
+
+//		for (int left = 1; left <= 5; left = left + 1) {
+//			for (int right = 1; right <= 10; right = right + 1) {
+//				g.drawLine(1, left, 10, right);
+//			}
+//		}
+
+//		for (int left = 3; left <= 7; left += 1) {
+//			for (int right = 1; right <=10; right += 1) {
+//			g2.drawLine(1, left, 10, right);
+//			}
+//		}
+
+//		for (int left = 1; left <= 10; left = left + 1) {
+//			for (int right = 1; right <= left; right = right + 1) {
+//				g.drawLine(1, left, 10, right);
+//			}
+//		}
+	}
+}
---
+++ ch05/DrawWindow.java
@@ -0,0 +1,21 @@
+package ch05;
+import javax.swing.*;
+import java.awt.*;
+
+public class DrawWindow extends JComponent {
+
+	public static void main(String[] args) {
+		JFrame venster = new JFrame();
+		JPanel inhoud = new JPanel();
+		ArtComponent kunst = new ArtComponent();
+		
+		inhoud.add(kunst);
+		
+		venster.setContentPane(inhoud);
+		venster.setTitle("Galerij");
+		venster.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		venster.setLocation(250, 100);
+		venster.pack();
+		venster.setVisible(true);
+	}
+}
