c6f0bf77493a1d466f765fa55ae0fa486b6e41e6
arq <arq@192.168.0.250>
2018-01-07 22:56:08 +0100

    Test with filehandling

---
+++ src/ch09/WriteMatchingLines.java
@@ -0,0 +1,47 @@
+package ch09;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.Scanner;
+
+public class WriteMatchingLines {
+
+	public static void main(String[] args) {
+		Scanner in = null;
+		PrintWriter out = null;
+		try {
+			in = new Scanner(new File("the-file-name.txt"));
+//			out = new PrintWriter("/Users/Arq/Documents/becker.txt");
+			FileWriter fw = new FileWriter("becker.txt", true);
+			out = new PrintWriter(fw);
+		} catch (FileNotFoundException ex) {
+			System.out.println(ex.getMessage());
+			System.out.println("in " + System.getProperty("user.dir"));
+			System.exit(1);
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+		while (in.hasNextLine()) {
+			String record = in.nextLine();
+			out.println(record);
+			System.out.println(record);
+		}
+		
+		in.close();
+		out.close();
+		try {
+			PrintWriter writer = new PrintWriter("/home/lejos/the-file-name.txt", "UTF-8");
+			writer.println("The first line");
+			writer.println("The second line");
+			writer.close();
+		} catch (IOException e) {
+			// do something
+		}
+	}
+
+}
